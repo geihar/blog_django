c566bd38486fedd0b10eb371e5798bb121fc79c7
geihar <l0635968488@gmail.com>
2020-04-05 06:50:51 +0200

    Add similar posts

--- blog/views.py
+++ blog/views.py
@@ -5,6 +5,7 @@ from .models import Post, Comment
 from django.views.generic import ListView
 from .forms import EmailPostForm, CommentForm
 from taggit.models import Tag
+from django.db.models import Count
 
 
 def post_list(request, tag_slug=None):
@@ -31,6 +32,7 @@ def post_list(request, tag_slug=None):
 def post_detail(request, year, month, day, post):
     post = get_object_or_404(Post, slug=post, status='published', publish__year=year,
                              publish__month=month, publish__day=day)
+
     comments = post.comments.filter(active=True)
     new_comment = None
     if request.method == 'POST':
@@ -41,8 +43,11 @@ def post_detail(request, year, month, day, post):
             new_comment.save()
     else:
         comment_form = CommentForm()
+    post_tags_ids = post.tags.values_list('id', flat=True)
+    similar_posts = Post.published.filter(tags__in=post_tags_ids).exclude(id=post.id)
+    similar_posts = similar_posts.annotate(same_tags=Count('tags')).order_by('-same_tags', '-publish')[:4]
     return render(request, 'blog/post/detail.html', {'post': post, 'comments': comments, 'new_comment': new_comment,
-                                                     'comment_form': comment_form})
+                                                     'comment_form': comment_form, 'similar_posts': similar_posts})
 
 
 class PostListView(ListView):
